35b16ddd944d5963583b68b36bc95d380369f7ec
JianmeiZhong <37535747+JianmeiZhong@users.noreply.github.com>
2023-12-03 18:19:35 +0800

    Delete R directory

--- R/utils.R
+++
@@ -1,9 +0,0 @@
-#' min-max normalize the matrix, columns are cells
-#' @param mat a matrix.
-#' @keywords internal
-#' @noRd
-.normalize <- function(x){
-  num <- x - min(x)
-  denom <- max(x) - min(x)
-  return(num/denom)
-}
